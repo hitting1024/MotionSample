f1bab8e42bd116cd7e83ffc20cae04e8bf3b21d5
IOKA <hit0apps@gmail.com>
2018-06-08 21:57:21 +0900

    initial commit

---
+++ MotionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MotionSample
+//
+//  Created by hitting on 2018/06/08.
+//  Copyright © 2018年 Hit Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
